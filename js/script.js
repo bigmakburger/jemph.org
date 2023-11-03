7bf37581161c49bf020093f92042d9e08b7d7a27
bigmakburger <143499641+bigmakburger@users.noreply.github.com>
2023-11-03 13:19:41 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,19 +8,5 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener('scroll', revealContent);
-});
-
-function revealContent() {
-    var section = document.getElementById('about');
-    var position = section.getBoundingClientRect().top;
-
-    if (position < window.innerHeight / 1.5) {
-        section.style.opacity = 1;
-        section.style.transform = 'translateX(0)';
-    }
-}
-
 
 
